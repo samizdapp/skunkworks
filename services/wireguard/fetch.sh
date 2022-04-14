bfa4b7fe9ecce4b453d4983b642424f84914c72c
Lachlan Hogg <41929615+HOGGL@users.noreply.github.com>
2022-04-14 10:36:00 +1000

    Use API to get latest dev version of balenaOS

--- services/wireguard/fetch.sh
+++ services/wireguard/fetch.sh
@@ -1,3 +1,8 @@
+VERSION=$(curl https://api.balena-cloud.com/device-types/v1/$1/images |
+	      # The latest development version, replacing + with %2B.
+	      jq -r 'first(.versions[] | select(endswith("dev"))) | sub("\\+"; "%2B")')
+curl -L https://files.balena-cloud.com/images/$1/$VERSION/kernel_modules_headers.tar.gz | tar xz
+
 KERNEL_RELEASE=$(cat kernel_modules_headers/include/config/kernel.release)
 dpkg --compare-versions $KERNEL_RELEASE ge 5.6 && exit 0
 
